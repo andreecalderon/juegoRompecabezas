c0e9cf377387acf8a5c61fe4df509e2111d00156
Calderon <jacalderon@scl.edu.gt>
2025-02-13 21:05:35 -0600

    se giran las cartas

--- components/tablero/tablero.js
+++ components/tablero/tablero.js
@@ -1,22 +1,57 @@
 import { barajeo } from "./data.js";
 
+let cartasVolteadas = [];
+let parejasEncontradas = 0;
+
 function item(contenido) {
     let div = document.createElement("div");
-    div.textContent = contenido;
     div.className = "carta";
+    div.dataset.valor = contenido;
+
+    let frente = document.createElement("div");
+    frente.className = "frente";
+    frente.textContent = contenido;
+
+    let dorso = document.createElement("div");
+    dorso.className = "dorso";
+    dorso.textContent = "?";
+
+    div.appendChild(frente);
+    div.appendChild(dorso);
+    div.addEventListener("click", () => manejarVolteo(div));
+
     return div;
 }
 
+function manejarVolteo(carta) {
+    if (cartasVolteadas.length < 2 && !carta.classList.contains("volteada")) {
+        carta.classList.add("volteada");
+        cartasVolteadas.push(carta);
 
-function cargar_cartas() {
-   
+        if (cartasVolteadas.length === 2) {
+            setTimeout(verificarPareja, 1000);
+        }
+    }
+}
+
+function verificarPareja() {
+    let [carta1, carta2] = cartasVolteadas;
+
+    if (carta1.dataset.valor === carta2.dataset.valor) {
+        parejasEncontradas++;
+    } else {
+        carta1.classList.remove("volteada");
+        carta2.classList.remove("volteada");
+    }
+
+    cartasVolteadas = [];
+}
+
+function cargar_cartas() {  
     let todas_las_cartas = barajeo();
-
-    
     let div = document.createElement("div");
     div.className = "divtablero";
-
- 
+    
     todas_las_cartas.forEach(contenido => {
         div.appendChild(item(contenido));
     });
